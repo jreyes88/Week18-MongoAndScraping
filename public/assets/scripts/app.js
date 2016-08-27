826e7e53ffe1303f5323cac03ad70829ff7236ee
Joey Reyes <jreyes88@gmail.com>
2016-08-27 14:49:29 -0500

    heroku deployment

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -11,11 +11,12 @@ $(document).on('click', '#mainButton', function(){
                 console.log(data);
                 // for each one
                 // ====================
+                $('#articles').append('<p>Click on Article Title to add a Comment!</p>');
                 for (var i = 0; i<data.length; i++)
                     {
                         // display the apropos information on the page
                         // ====================
-                        $('#articles').append('<p>Click on Article Title to add a Comment!</p><p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
+                        $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
                     }
             }
         );
